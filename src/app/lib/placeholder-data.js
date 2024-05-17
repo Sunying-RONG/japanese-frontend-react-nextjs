2c75cc863f505c14ec0b2591090031fe4fac448f
Sunying <sunying.rong@hotmail.com>
2024-05-17 12:54:29 -0400

    init

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,232 @@
+const Monographs = [
+    {
+        pronounce: 'a',
+        hiragana: 'あ',
+        katakana: 'ア'
+    },
+    {
+        pronounce: 'i',
+        hiragana: 'い',
+        katakana: 'イ'
+    },
+    {
+        pronounce: 'u',
+        hiragana: 'う',
+        katakana: 'ウ'
+    },
+    {
+        pronounce: 'e',
+        hiragana: 'え',
+        katakana: 'エ'
+    },
+    {
+        pronounce: 'o',
+        hiragana: 'お',
+        katakana: 'オ'
+    },
+    {
+        pronounce: 'ka',
+        hiragana: 'か',
+        katakana: 'カ'
+    },
+    {
+        pronounce: 'ki',
+        hiragana: 'き',
+        katakana: 'キ'
+    },
+    {
+        pronounce: 'ku',
+        hiragana: 'く',
+        katakana: 'ク'
+    },
+    {
+        pronounce: 'ke',
+        hiragana: 'け',
+        katakana: 'ケ'
+    },
+    {
+        pronounce: 'ko',
+        hiragana: 'こ',
+        katakana: 'コ'
+    },
+    {
+        pronounce: 'sa',
+        hiragana: 'さ',
+        katakana: 'サ'
+    },
+    {
+        pronounce: 'shi',
+        hiragana: 'し',
+        katakana: 'シ'
+    },
+    {
+        pronounce: 'su',
+        hiragana: 'す',
+        katakana: 'ス'
+    },
+    {
+        pronounce: 'se',
+        hiragana: 'せ',
+        katakana: 'セ'
+    },
+    {
+        pronounce: 'so',
+        hiragana: 'そ',
+        katakana: 'ソ'
+    },
+    {
+        pronounce: 'ta',
+        hiragana: 'た',
+        katakana: 'タ'
+    },
+    {
+        pronounce: 'chi',
+        hiragana: 'ち',
+        katakana: 'チ'
+    },
+    {
+        pronounce: 'tsu',
+        hiragana: 'つ',
+        katakana: 'ツ'
+    },
+    {
+        pronounce: 'te',
+        hiragana: 'て',
+        katakana: 'テ'
+    },
+    {
+        pronounce: 'to',
+        hiragana: 'と',
+        katakana: 'ト'
+    },
+    {
+        pronounce: 'na',
+        hiragana: 'な',
+        katakana: 'ナ'
+    },
+    {
+        pronounce: 'ni',
+        hiragana: 'に',
+        katakana: '二'
+    },
+    {
+        pronounce: 'nu',
+        hiragana: 'ぬ',
+        katakana: 'ヌ'
+    },
+    {
+        pronounce: 'ne',
+        hiragana: 'ね',
+        katakana: 'ネ'
+    },
+    {
+        pronounce: 'no',
+        hiragana: 'の',
+        katakana: 'ノ'
+    },
+    {
+        pronounce: 'ha',
+        hiragana: 'は',
+        katakana: 'ハ'
+    },
+    {
+        pronounce: 'hi',
+        hiragana: 'ひ',
+        katakana: 'ヒ'
+    },
+    {
+        pronounce: 'fu',
+        hiragana: 'ふ',
+        katakana: 'フ'
+    },
+    {
+        pronounce: 'he',
+        hiragana: 'へ',
+        katakana: 'ヘ'
+    },
+    {
+        pronounce: 'ho',
+        hiragana: 'ほ',
+        katakana: 'ホ'
+    },
+    {
+        pronounce: 'ma',
+        hiragana: 'ま',
+        katakana: 'マ'
+    },
+    {
+        pronounce: 'mi',
+        hiragana: 'み',
+        katakana: 'ミ'
+    },
+    {
+        pronounce: 'mu',
+        hiragana: 'む',
+        katakana: 'ム'
+    },
+    {
+        pronounce: 'me',
+        hiragana: 'め',
+        katakana: 'メ'
+    },
+    {
+        pronounce: 'mo',
+        hiragana: 'も',
+        katakana: 'モ'
+    },
+    {
+        pronounce: 'ya',
+        hiragana: 'や',
+        katakana: 'ヤ'
+    },
+    {
+        pronounce: 'yu',
+        hiragana: 'ゆ',
+        katakana: 'ユ'
+    },
+    {
+        pronounce: 'yo',
+        hiragana: 'よ',
+        katakana: 'ヨ'
+    },
+    {
+        pronounce: 'ra',
+        hiragana: 'ら',
+        katakana: 'ラ'
+    },
+    {
+        pronounce: 'ri',
+        hiragana: 'り',
+        katakana: 'リ'
+    },
+    {
+        pronounce: 'ru',
+        hiragana: 'る',
+        katakana: 'ル'
+    },
+    {
+        pronounce: 're',
+        hiragana: 'れ',
+        katakana: 'レ'
+    },
+    {
+        pronounce: 'ro',
+        hiragana: 'ろ',
+        katakana: 'ロ'
+    },
+    {
+        pronounce: 'wa',
+        hiragana: 'わ',
+        katakana: 'ワ'
+    },
+    {
+        pronounce: 'wo',
+        hiragana: 'を',
+        katakana: 'ヲ'
+    },
+    {
+        pronounce: 'n',
+        hiragana: 'ん',
+        katakana: 'ン'
+    }
+]
