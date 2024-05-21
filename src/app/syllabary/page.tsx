4054294cdc58db9a84ee44a4a179b0e64e27d9f4
Sunying <sunying.rong@hotmail.com>
2024-05-21 16:11:04 -0400

    extract customized grid

--- src/app/syllabary/page.tsx
+++ src/app/syllabary/page.tsx
@@ -1,7 +1,5 @@
-"use client"
 import { monographs } from '@/app/data/placeholder-data';
-import { KanaSelectContext, KanaSelectContextType } from '../context/kana-select-context';
-import { useContext } from 'react';
+import KanaGrid from '@/app/ui/syllabary/kana-grid';
 
 const monographsWithPlaceholder = monographs;
 for (let index = 0; index < monographsWithPlaceholder.length; index++) {
@@ -18,30 +16,11 @@ for (let index = 0; index < monographsWithPlaceholder.length; index++) {
 }
 
 export default function Page() {
-    const { showOrHide } = useContext(KanaSelectContext) as KanaSelectContextType;
     return(
-        <>
-            <h1 className='text-center text-3xl'>Monographs (gojūon)</h1>
-            <div className='w-fit grid gap-2 lg:gap-8 grid-cols-5 content-around mx-auto my-10'>
-                {monographsWithPlaceholder.map(item => {
-                    if (item.pronounce) {
-                        return <div
-                            key={item.id}
-                            className='mx-auto flex flex-col'
-                        >
-                            <div className='flex flex-row'>
-                                <div className={`${showOrHide!.h ? 'block' : 'hidden'} mx-auto bg-rose-200 hover:bg-rose-300 text-base md:text-3xl p-2`}>{item.hiragana}</div>
-                                <div className={`${showOrHide!.k ? 'block' : 'hidden'} mx-auto bg-sky-200 hover:bg-sky-300 text-base md:text-3xl p-2`}>{item.katakana}</div>
-                            </div>
-                            <div className={`${showOrHide!.p ? 'block' : 'hidden'} w-full bg-purple-100 text-sm md:text-xl p-1`}>
-                                <p className='text-center'>{item.pronounce}</p>
-                            </div>
-                        </div>
-                    } else {
-                        return <div key={item.id}></div>
-                    }
-                })}
-            </div>
-        </>
+        <KanaGrid 
+            title='Monographs (gojūon)' 
+            kanaData={monographsWithPlaceholder} 
+            columnNb={5} 
+        />
     ); 
 }
--- src/app/ui/syllabary/kana-grid.tsx
+++ src/app/ui/syllabary/kana-grid.tsx
@@ -9,18 +9,22 @@ export default function KanaGrid(props: { title: string; kanaData: any[]; column
             <h1 className='text-center text-3xl'>{props.title}</h1>
             <div className={`w-fit grid gap-2 lg:gap-8 grid-cols-${props.columnNb} content-around mx-auto my-10`}>
                 {props.kanaData.map(item => {
-                    return <div
-                        key={item.id}
-                        className='mx-auto flex flex-col'
-                    >
-                        <div className='flex flex-row'>
-                            <div className={`${showOrHide!.h ? 'block' : 'hidden'} mx-auto bg-rose-200 hover:bg-rose-300 text-base md:text-3xl p-2`}>{item.hiragana}</div>
-                            <div className={`${showOrHide!.k ? 'block' : 'hidden'} mx-auto bg-sky-200 hover:bg-sky-300 text-base md:text-3xl p-2`}>{item.katakana}</div>
+                    if (item.pronounce) {
+                        return <div
+                            key={item.id}
+                            className='mx-auto flex flex-col'
+                        >
+                            <div className='flex flex-row'>
+                                <div className={`${showOrHide!.h ? 'block' : 'hidden'} mx-auto bg-rose-200 hover:bg-rose-300 text-base md:text-3xl p-2`}>{item.hiragana}</div>
+                                <div className={`${showOrHide!.k ? 'block' : 'hidden'} mx-auto bg-sky-200 hover:bg-sky-300 text-base md:text-3xl p-2`}>{item.katakana}</div>
+                            </div>
+                            <div className={`${showOrHide!.p ? 'block' : 'hidden'} w-full bg-purple-100 text-sm md:text-xl p-1`}>
+                                <p className='text-center'>{item.pronounce}</p>
+                            </div>
                         </div>
-                        <div className={`${showOrHide!.p ? 'block' : 'hidden'} w-full bg-purple-100 text-sm md:text-xl p-1`}>
-                            <p className='text-center'>{item.pronounce}</p>
-                        </div>
-                    </div>
+                    } else {
+                        return <div key={item.id}></div>
+                    }
                 })}
             </div>
         </>
